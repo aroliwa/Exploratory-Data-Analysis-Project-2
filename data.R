e9c7761290f646f247f91cd6ae0fa9c002153eeb
aroliwa <aroliwa@gmail.com>
2018-01-03 12:38:39 +0100

    Update data.R

--- data.R
+++ data.R
@@ -4,3 +4,4 @@ download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_d
 unzip("data.zip")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
